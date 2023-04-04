5c8013cdd434aed672ab1ec6c3ea83c7f8b790c5
zaticpetru <zatic.petru@outlook.com>
2023-04-04 17:34:38 +0300

    Created color scheme Setup general CSS stylings Basic navigation styling

--- src/components/Layout/MainNavigation.tsx
+++ src/components/Layout/MainNavigation.tsx
@@ -1,16 +1,22 @@
 import { AuthContext } from "@/pages/_app";
 import Link from "next/link";
-import { useContext } from "react";
-
-import classes from "./MainNavigation.module.css";
+import { useContext, useState } from "react";
 
 const MainNavigation = () => {
   const { removeUserInfoFromLocalStorage, token } = useContext(AuthContext);
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <header className={classes.header}>
-      <Link href="/">Home</Link>
-      <nav>
-        <ul>
+    <header className="header rad-shadow justify-space-around font-size-m font-weight-600 p-1">
+      <button className="p-1 btn-link text-1 menu-button" onClick={() => setOpen(!isOpen)}>
+        <i className="gg-menu-left-alt"></i>
+      </button>
+      <nav className="margin-inline-auto" data-visible={`${isOpen}`}>
+        <ul className="flex justify-space-around gap-2 no-bullet display-block menu-items">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
           {!token && (
             <li>
               <Link href="/auth">Login</Link>
@@ -22,7 +28,7 @@ const MainNavigation = () => {
                 <Link href="/profile">Profile</Link>
               </li>
               <li>
-                <button onClick={() => removeUserInfoFromLocalStorage()}>
+                <button className="font-size-m font-weight-600 text-1 btn-link  " onClick={() => removeUserInfoFromLocalStorage()}>
                   Logout
                 </button>
               </li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,61 @@
-import Head from "next/head";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Quiz App</title>
-        <meta name="description" content="Quiz app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="flex gap-0 direction-column p-1 m-1 surface-2 rad-shadow">
+        <h1 className="text-align-center text-1">Quiz App</h1>
+        <div className="flex justify-center">
+          <input className="text-2 text-align-center font-size-m search-field" type="text" placeholder="Search..."></input>
+          <button className="p-1 btn-link text-1 search-button">
+            <i className="gg-search"></i>
+          </button>
+        </div>
+      </div>
+      <div className="flex grow gap-2 m-1">
+        <div className="brand rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="accent rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+      </div>
+      <div className="flex wrap grow m-1 surface-demo">
+        <div className="surface-1 rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="surface-2 rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="surface-3 rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="surface-4 rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+      </div>
+      <div className="flex wrap grow colors-demo m-1">
+        <div className="info rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="success rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="warning rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+        <div className="failure rad-shadow p-1">
+          <h1 className="text-1">Text1</h1>
+          <h1 className="text-2">Text2</h1>
+        </div>
+      </div>
       <main style={{ display: "flex", flexDirection: "column" }}>
         {/* <Link href="/login">Log in</Link>
         <Link href="/signup">Sign up</Link>
