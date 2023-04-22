402338fa8a8e39ec17679a519e55fdbadc0e5a3d
zaticpetru <zatic.petru@outlook.com>
2023-04-22 20:08:14 +0300

    Add styling to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,8 @@
 const PageNotFound = () => {
-  return <div>Ooops, can't find this page</div>;
+  return <div className="flex direction-column gap-3 justify-center text-align-center m-y-2 ">
+    <div className="font-size-l font-weight-600 failure p-2 rad-shadow">404</div>
+    <div className="p-1 info rad-shadow">Ooops, can&#39;t find this page</div>
+  </div>;
 };
 
 export default PageNotFound;
