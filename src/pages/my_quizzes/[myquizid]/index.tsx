8216631813d9dda32ba2f4ca43178bd543e01b85
Catalina <catalinahasnas1996@gmail.com>
2023-04-26 16:32:24 +0300

    display data on my quiz page

--- src/pages/my_quizzes/[myquizid]/index.tsx
+++ src/pages/my_quizzes/[myquizid]/index.tsx
@@ -35,9 +35,12 @@ const MyQuizPage = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <div>My Quiz page</div>
       {isLoading && <p>is loading...</p>}
-      {data && <p> {data.data.quiz.description} </p>}
+      {data && (
+        <>
+          <p> {data.data.quiz.title} </p> <p> {data.data.quiz.description} </p>
+        </>
+      )}
       <Link href={`/edit_quiz/${router.query.myquizid}`}>Edit/ View quiz</Link>
       <Link href={`/my_quizzes/${router.query.myquizid}/responses`}>
         View Responses
