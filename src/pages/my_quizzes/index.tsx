f71d4eb0616b6e425ba80cfea2b7f348f80b4045
Catalina <catalinahasnas1996@gmail.com>
2023-06-12 21:17:06 +0300

    add responses to my quizzes page

--- src/pages/my_quizzes/index.tsx
+++ src/pages/my_quizzes/index.tsx
@@ -3,10 +3,16 @@ import useSWR from "swr";
 import { useContext } from "react";
 import { QuizModelWithId } from "@/models/quiz";
 import { AuthContext } from "@/pages/_app";
+import { ResponseModelWIthId } from "@/models/response";
+
+type ResponseType = Pick<ResponseModelWIthId, "_id" | "reviewed"> & {
+  quiz_title: string;
+};
 
 interface MyQuizzesResponse {
   data: {
     quizzes: Pick<QuizModelWithId, "_id" | "title">[];
+    responses: ResponseType[];
   };
 }
 
@@ -37,15 +43,44 @@ const MyQuizzesPage = () => {
     return <p>Error!</p>;
   }
 
+  console.log(data?.data);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div>My Quizzes page</div>
-      {data?.data.quizzes &&
-        data?.data.quizzes.map((quiz) => (
-          <Link key={quiz._id} href={`/my_quizzes/${quiz._id}`}>
-            {quiz.title}
-          </Link>
-        ))}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gap: "16px",
+      }}
+    >
+      <div style={{ gridColumn: "1 / span 2" }}>My Quizzes page</div>
+      <div
+        style={{ gridColumn: "1", display: "flex", flexDirection: "column" }}
+      >
+        <p>Quizzes that I have created</p>
+        {data?.data.quizzes &&
+          data?.data.quizzes.map((quiz) => (
+            <Link key={quiz._id} href={`/my_quizzes/${quiz._id}`}>
+              {quiz.title}
+            </Link>
+          ))}
+      </div>
+
+      <div
+        style={{ gridColumn: "2", display: "flex", flexDirection: "column" }}
+      >
+        {data?.data.responses &&
+          data?.data.responses.map((response) => (
+            <>
+              <p>Quizzes that I have responded to</p>
+
+              <Link key={response._id} href={`/response/${response._id}`}>
+                {response.quiz_title}
+              </Link>
+              <p> {response.reviewed ? "reviewed" : "not reviewed yet"} </p>
+            </>
+          ))}
+      </div>
     </div>
   );
 };
